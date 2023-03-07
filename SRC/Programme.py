7e6074cd5dc87edc5492e1ef283394d125e1e00b
choubael <noutcheuchoubael@gmail.com>
2023-03-07 11:20:38 +0100

    recent

--- SRC/Programme.py
+++ SRC/Programme.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from tkinter import Label, StringVar,Tk,Canvas,Text,Button,PhotoImage
 from tkinter.constants import END,WORD
+from sklearn import svm
+import seaborn as sns
 import pickle
 
 #chemin d'accès aux assets
@@ -10,7 +12,7 @@ def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
 window = Tk()
-window.title("TP3 FAKE NEWS")
+window.title("TP4 LOAN")
 width = 600 
 height = 300
  
@@ -28,14 +30,20 @@ result=StringVar()
 result.set("------")
 
 #import de notre modele SVM
-with open('PassiveAggressiveClassifier.pkl', 'rb') as f:
-    pac = pickle.load(f)
-with open('TfidfVectorizer.pkl', 'rb') as f:
-    tf = pickle.load(f)
+with open('Loan_classifier.pkl', 'rb') as f:
+    model = pickle.load(f)
+    
 
-def analyser():    
-    inputs=[entry.get(0.0,END)]
-    result.set(pac.predict(tf.transform(inputs)))
+def predire():
+    ok=False
+    try:
+        inputs=[[float(LA.get(0.0,END)),float(CH.get(0.0,END))]]
+        ok=True
+    except:
+        result.set('Entrer des bonnes Valeurs')
+        ok=False
+    if ok:
+        result.set(model.predict(inputs))
         
     
 #arrière plan
@@ -58,8 +66,7 @@ bg = canvas.create_image(
     image=bg_image
     )
 
-
-entry = Text(
+LA = Text(
     canvas,
     bd=0,
     bg="#FFFFFF",
@@ -69,28 +76,44 @@ entry = Text(
     cursor="dot",
 
 )
-entry.place(
-    x=70,
-    y=90,
-    width=450,
-    height=93
+LA.place(
+    x=155,
+    y=113,
+    width=110,
+    height=20
+)
+CH = Text(
+    canvas,
+    bd=0,
+    bg="#FFFFFF",
+    highlightthickness=0,
+    font=("sergio",10,"bold"),
+    wrap=WORD,
+    cursor="dot",
+
+)
+CH.place(
+    x=325,
+    y=113,
+    width=110,
+    height=20
 )
 
 button_image = PhotoImage(
     file=relative_to_assets("btn.png"))
 button_1 = Button(
     canvas,
-    command=analyser,
+    command=predire,
     image=button_image,
     borderwidth=0,
     highlightthickness=0,
     relief="flat",
-    activebackground="#004C7F",
-    bg="#004C7F"
+    activebackground="#9a2b2b",
+    bg="#9a2b2b"
 )
 button_1.place(
-    x=260,
-    y=200,
+    x=266,
+    y=160,
     width=69,
     height=26
 )
@@ -104,7 +127,7 @@ resultat=Label(
 )
 resultat.place(
         x=150,
-        y=255,
+        y=230,
         width=300
 )
 
